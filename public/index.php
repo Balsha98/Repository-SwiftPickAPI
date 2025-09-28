772766b078e6e8e5d7b974bbfac35b88d72eb110
Balša Bazović <balsa.bazovic@gmail.com>
2025-09-28 09:01:46 -0400

    Updated index file.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-print_r($_GET['uri'] ?? 'TEST');
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebDev | SwiftPick</title>
+</head>
+
+<body>
+    <h1>SwiftPickAPI</h1>
+</body>
+
+</html>
