88089877ce5c2b8b5523814e635053369afcac16
Balša Bazović <balsa.bazovic@gmail.com>
2025-09-28 18:05:24 -0400

    Updated index files.

--- public/api/index.php
+++ public/api/index.php
@@ -3,26 +3,54 @@
 require_once __DIR__ . '/source/autoload.php';
 
 // Imported classes.
-use Source\Classes\{Router, Routes};
-use Source\Helpers\Classes\{Debugger};
+use Source\Classes\Routes;
+use Source\Helpers\Classes\{Debugger, Encoder};
 
 try {
     $allowedRoutes = [];
     $method = $_SERVER['REQUEST_METHOD'];
 
-    if (!Routes::$routes[$method]) {
+    if (is_null(Routes::$routes[$method])) {
         throw new Exception('Request method is not supported!');
     }
 
     $allowedRoutes = Routes::$routes[$method];
     $uriParts = explode('/', $_SERVER['REQUEST_URI']);
-    $partsLength = count($uriParts);
+    $resourceName = $uriParts[2];
+    $numUriParts = count($uriParts);
 
     // Check if content file exists.
-    $filePath = __DIR__ . '/assets/content/' . $uriParts[2] . '.json';
+    $filePath = __DIR__ . "/assets/content/{$resourceName}.json";
     if (!file_exists($filePath)) {
         throw new Exception('Unidentifiable resource given...');
     }
+
+    $targetContent = file_get_contents($filePath);
+
+    // Check for ID.
+    if ($numUriParts === 4) {
+        $resourceID = $uriParts[3];
+        $targetData = Encoder::decodeFromJSON($targetContent);
+        $itemIDs = [];
+
+        foreach ($targetData as $data) {
+            if ((int) $data['id'] === (int) $resourceID) {
+                $targetContent = Encoder::encodeToJSON($data);
+            }
+
+            $itemIDs[] = (int) $data['id'];
+        }
+
+        // Check if ID exists.
+        if (!in_array($resourceID, $itemIDs)) {
+            throw new Exception("ResourceID({$resourceID}) does not exist...");
+        }
+    }
 } catch (Exception $e) {
     echo $e->getMessage();
+
+    die();
 }
+
+// Print result.
+echo $targetContent;
