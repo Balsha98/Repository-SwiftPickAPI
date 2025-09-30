9be7c1d7ef7c0fcb359a5c5de28206c1fb7f5f0e
Balša Bazović <balsa.bazovic@gmail.com>
2025-09-30 16:32:43 -0400

    Updated index file.

--- public/api/index.php
+++ public/api/index.php
@@ -4,20 +4,32 @@ require_once __DIR__ . '/source/autoload.php';
 
 // Imported classes.
 use Source\Classes\Routes;
-use Source\Helpers\Classes\{Debugger, Encoder};
+// use Source\Helpers\Classes\Debugger;
+use Source\Helpers\Classes\Encoder;
 
 try {
     $allowedRoutes = [];
     $method = $_SERVER['REQUEST_METHOD'];
 
+    // Set appropriate response header.
+    header('Content-Type: application/json');
+
+    // Guard clause.
     if (is_null(Routes::$routes[$method])) {
         throw new Exception('Request method is not supported!');
     }
 
     $allowedRoutes = Routes::$routes[$method];
     $uriParts = explode('/', $_SERVER['REQUEST_URI']);
-    $resourceName = $uriParts[2];
     $numUriParts = count($uriParts);
+
+    // Guard clause.
+    if ($numUriParts < 3) {
+        throw new Exception('Please specify a resource...');
+    }
+
+    // Get targeted resource.
+    $resourceName = $uriParts[2];
 
     // Check if content file exists.
     $filePath = __DIR__ . "/assets/content/{$resourceName}.json";
@@ -25,32 +37,33 @@ try {
         throw new Exception('Unidentifiable resource given...');
     }
 
-    $targetContent = file_get_contents($filePath);
+    $targetData = Encoder::decodeFromJSON(file_get_contents($filePath));
 
     // Check for ID.
     if ($numUriParts === 4) {
         $resourceID = $uriParts[3];
-        $targetData = Encoder::decodeFromJSON($targetContent);
+
         $itemIDs = [];
-
         foreach ($targetData as $data) {
-            if ((int) $data['id'] === (int) $resourceID) {
-                $targetContent = Encoder::encodeToJSON($data);
-            }
-
             $itemIDs[] = (int) $data['id'];
+
+            if ((int) $data['id'] === (int) $resourceID) {
+                $targetData = $data;
+
+                break;
+            }
         }
 
         // Check if ID exists.
         if (!in_array($resourceID, $itemIDs)) {
-            throw new Exception("ResourceID({$resourceID}) does not exist...");
+            throw new Exception("{$resourceName}/{$resourceID} does not exist...");
         }
     }
 } catch (Exception $e) {
-    echo $e->getMessage();
+    echo Encoder::encodeToJSON('error', $e->getMessage());
 
     die();
 }
 
 // Print result.
-echo $targetContent;
+echo Encoder::encodeToJSON('success', $targetData);
