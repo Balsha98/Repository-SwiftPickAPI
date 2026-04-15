31958b9de716deaedcfcad1a42089f0d9898c84b
Balša Bazović <balsa.bazovic@gmail.com>
2026-04-14 23:08:50 -0400

    Updated response structure.

--- swift-pick-api/public/api/source/helpers/classes/Encoder.php
+++ swift-pick-api/public/api/source/helpers/classes/Encoder.php
@@ -6,7 +6,7 @@ class Encoder
 {
     public static function encodeToJSON(string $key, mixed $data)
     {
-        return json_encode([$key => $data]);
+        return json_encode($data);
     }
 
     public static function decodeFromJSON(string $json, bool $isAssoc = true)
